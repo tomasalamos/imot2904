382f101c26fd68c469ad9b097854873b0a056f7e
tomasalamos <tomas.alamos@uc.cl>
2025-04-29 18:18:17 -0400

    Cambios en la estructura de la app y archivos estáticos

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def download_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
