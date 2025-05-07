e390064f4144e1b564e172bf8c0fdda41ca457e7
tomasalamos <tomas.alamos@uc.cl>
2025-05-06 23:26:01 -0400

    prueba 2

--- app.py
+++ app.py
@@ -70,7 +70,6 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'user' not in session:
-            flash('You must log in to access.', 'warning')
             return redirect(url_for('auth'))
         return f(*args, **kwargs)
     return decorated_function
